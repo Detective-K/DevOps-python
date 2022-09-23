b228b9788e3f413339bc1e5806365f33f093584a
Detective-K <asfako1122@gmail.com>
2022-09-23 14:53:40 +0800

    新增測試程式

---
+++ testPython.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 27 11:39:26 2022
+
+@author: d02044
+"""
+import torch
+import torchvision
+from torchvision import transforms, utils
+import matplotlib.pyplot as plt
+
+
+def show_batch(imgs):
+   grid = utils.make_grid(imgs,nrow=5)
+   plt.imshow(grid.numpy().transpose((1, 2, 0)))
+   plt.title('Batch from dataloader')
+
+
+def pytorch1():
+    a = 1
+    img_data  = torchvision.datasets.ImageFolder( 'D:/Thesis/test',
+                                                transform=transforms.Compose([
+                                                    transforms.Resize(256),
+                                                    transforms.CenterCrop(224),
+                                                    transforms.ToTensor()])
+                                                )
+    
+    print(len(img_data))
+    data_loader = torch.utils.data.DataLoader(img_data, batch_size=20,shuffle=True)
+    print(len(data_loader))    
+    
+    for i, (batch_x, batch_y) in enumerate(data_loader):
+        if(i<4):
+            print(i, batch_x.size(), batch_y.size())
+    
+            show_batch(batch_x)
+            plt.axis('off')
+            plt.show()
+            
+def trya():
+    x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)  # x data (tensor), shape=(100, 1)
+    y = x.pow(2) + 0.2*torch.rand(x.size())                 # noisy y data (tensor), shape=(100, 1)
+    
+    # 画图
+    plt.scatter(x.data.numpy(), y.data.numpy())
+    plt.show()
+
+            
+        
+        
+if __name__ =="__main__": trya()
