a6179e4ef912976005994716d7b496342ee52548
Detective-K <asfako1122@gmail.com>
2022-11-08 17:26:01 +0800

    清理資料

--- testPython.py
+++ testPython.py
@@ -71,29 +71,42 @@ import numpy as np
 
 
 
-# train_file = bz2.BZ2File('D:/Testdata/train.ft.txt.bz2')
-# test_file = bz2.BZ2File('D:/Testdata/test.ft.txt.bz2')
-# train_file = train_file.readlines()
-# test_file = test_file.readlines()
-# print(train_file[0])
+train_file = bz2.BZ2File('D:/Testdata/train.ft.txt.bz2')
+test_file = bz2.BZ2File('D:/Testdata/test.ft.txt.bz2')
+train_file = train_file.readlines()
+test_file = test_file.readlines()
+print(train_file[0])
 
-# num_train = 800000
-# num_test = 200000
+num_train = 800000
+num_test = 200000
 
-# train_file = [x.decode('utf-8') for x in train_file[:num_train]]
-# test_file = [x.decode('utf-8') for x in test_file[:num_test]]
+train_file = [x.decode('utf-8') for x in train_file[:num_train]]
+test_file = [x.decode('utf-8') for x in test_file[:num_test]]
 
-# train_labels = [0 if x.split(' ')[0] == '__label__1' else 1 for x in train_file]
-# test_labels = [0 if x.split(' ')[0] == '__label__1' else 1 for x in test_file]
+# 将__label__1编码为0（差评），__label__2编码为1（好评）
+train_labels = [0 if x.split(' ')[0] == '__label__1' else 1 for x in train_file]
+test_labels = [0 if x.split(' ')[0] == '__label__1' else 1 for x in test_file]
+
+"""
+`split(' ', 1)[1]`：将label和data分开后，获取data部分
+`[:-1]`：去掉最后一个字符(\n)
+`lower()`: 将其转换为小写，因为区分大小写对情感识别帮助不大，且会增加编码难度
+"""
+train_sentences = [x.split(' ', 1)[1][:-1].lower() for x in train_file]
+test_sentences = [x.split(' ', 1)[1][:-1].lower() for x in test_file]
+
+for i in range(len(train_sentences)):
+    train_sentences[i] = re.sub('\d','0',train_sentences[i])
+
+for i in range(len(test_sentences)):
+    test_sentences[i] = re.sub('\d','0',test_sentences[i])
 
 
-# train_sentences = [x.split(' ', 1)[1][:-1].lower() for x in train_file]
-# test_sentences = [x.split(' ', 1)[1][:-1].lower() for x in test_file]
+for i in range(len(train_sentences)):
+    if 'www.' in train_sentences[i] or 'http:' in train_sentences[i] or 'https:' in train_sentences[i] or '.com' in train_sentences[i]:
+        train_sentences[i] = re.sub(r"([^ ]+(?<=\.[a-z]{3}))", "<url>", train_sentences[i])
+        
+for i in range(len(test_sentences)):
+    if 'www.' in test_sentences[i] or 'http:' in test_sentences[i] or 'https:' in test_sentences[i] or '.com' in test_sentences[i]:
+        test_sentences[i] = re.sub(r"([^ ]+(?<=\.[a-z]{3}))", "<url>", test_sentences[i])
 
-
-
-rnn = nn.LSTM(10, 20, 2)
-input = torch.randn(5, 3, 10)
-h0 = torch.randn(2, 3, 20)
-c0 = torch.randn(2, 3, 20)
-output, (hn, cn) = rnn(input, (h0, c0))
