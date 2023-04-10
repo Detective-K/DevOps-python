fd680adfa1b5c87db7bc43718912cffd01918168
Detective-K <asfako1122@gmail.com>
2023-04-10 17:07:22 +0800

    增加合併資料

--- testPython.py
+++ testPython.py
@@ -27,11 +27,14 @@ def func(x):
     return s['comment'][0]
 
 def main():
-    #MLOPS取資料
+    #取StackOverflow各項資料
     MLOPS_df = pd.read_excel("D:\Thesis\Data\SOF\MLOPS_SOF.xlsx", engine='openpyxl')
     MLOPS_df['activity.question.postCell'] = MLOPS_df['activity.question.postCell'].apply(lambda x: func(x))
-    MLOPS_df = pd.read_excel("D:\Thesis\Data\SOF\MLOPS_SOF.xlsx", engine='openpyxl')
-    MLOPS_df['activity.question.postCell'] = MLOPS_df['activity.question.postCell'].apply(lambda x: func(x))
+    DevOps_df = pd.read_excel("D:\Thesis\Data\SOF\DevOps_SOF.xlsx", engine='openpyxl')
+    DevOps_df['activity.question.postCell'] = DevOps_df['activity.question.postCell'].apply(lambda x: func(x))
+    AutomatedModelDeployment_df = pd.read_excel("D:\Thesis\Data\SOF\AutomatedModelDeployment_SOF.xlsx", engine='openpyxl')
+    DevOps_df['activity.question.postCell'] = DevOps_df['activity.question.postCell'].apply(lambda x: func(x))
 
+    pd.concat([MLOPS_df['activity.question.postCell'], DevOps_df['activity.question.postCell']])
     return 0
 if __name__ == "__main__": main()
